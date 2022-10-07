fdfd83a10dc85b2732fe77baa6c9aa428dab0f83
Patrick da Silva Lara <desnecessauro000@gmail.com>
2022-10-07 15:39:45 -0300

    Primeiro Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express();
+const port = 8080;
+//Estou informando para o Express usar o EJS como View Engine
+app.set('view engine','ejs');
+app.use(express.static('public'))
+
+app.get('/',function(req, res){
+    
+
+ 
+
+    res.render('home',{
+    
+        
+    });
+});
+
+app.listen(port,function(error) {
+    if(error){
+        console.log('Houve um ERRO na hora de iniciar o servidor');
+
+    }
+    console.log('Servidor funcionando corretamente');
+});
